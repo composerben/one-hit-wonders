7eb2853f6e6b1e409da49c42ad9aac3d506ec78b
composerben <56130322+composerben@users.noreply.github.com>
2021-07-02 08:32:02 -0700

    kits correctly displaying (I think??)

--- react-app/src/components/Kit/Kit.js
+++ react-app/src/components/Kit/Kit.js
@@ -2,19 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import "./kit.css";
 
-function Kit() {
-  const userKits = useSelector((state) => Object.values(state.kitReducer.byId));
-  console.log(userKits);
+function Kit({kit}) {
+  // const userKits = useSelector((state) => Object.values(state.kitReducer.byId));
 
-  const kitComponents = userKits?.map((kit) => {
+  // const kitComponents = userKits?.map((kit) => {
     return (
-      <div key={kit.id} className="kit-card">
+      <div className="kit-card">
         <h2>{kit.name}</h2>
         <img alt={`${kit.name} Kit cover`} src={kit.cover_img_url}></img>
       </div>
     );
-  });
-  return kitComponents;
+  ;
+  // return kitComponents;
 }
 
 export default Kit;
--- react-app/src/components/User/User.js
+++ react-app/src/components/User/User.js
@@ -11,7 +11,8 @@ function User() {
   const dispatch = useDispatch();
   const { userId } = useParams();
   const user = useSelector((state) => state.userReducer);
-  console.log("Use selector USER", user);
+  const userKits = user.byId[userId]?.kits;
+  console.log(userKits);
 
   useEffect(() => {
     if (!userId) {
@@ -25,6 +26,14 @@ function User() {
     return null;
   }
 
+  const userKitComponents = userKits?.map((kit) => {
+    return (
+      <div key={kit.id}>
+        <Kit kit={kit} />
+      </div>
+    );
+  });
+
   return (
     <ul>
       <li>
@@ -36,9 +45,7 @@ function User() {
       <li>
         <strong>Email</strong> {user.email}
       </li>
-      <div className="kits-container">
-        <Kit />
-      </div>
+      <div className="kits-container">{userKitComponents}</div>
     </ul>
   );
 }
