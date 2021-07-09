918a1ef07124b226209cef719d08aab662d40a5b
composerben <56130322+composerben@users.noreply.github.com>
2021-07-09 08:48:59 -0700

    test for heroku

---
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SplashPage = () => {
+  return (
+    <>
+      <h1 className="welcome-text">Welcome to One Hit Wonders</h1>
+      <h3>The site dedicated to making noise and bothering your neighbors</h3>
+    </>
+  );
+};
+
+export default SplashPage;
