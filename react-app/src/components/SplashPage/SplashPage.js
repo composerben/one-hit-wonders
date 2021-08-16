b3d6a17be098ab76f9a19a3f864d2c8cbe9b4fcb
composerben <56130322+composerben@users.noreply.github.com>
2021-08-16 12:21:04 -0700

    finish styling splash page

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -8,7 +8,17 @@ const SplashPage = () => {
       <h3 className="subtitle">
         The site dedicated to making noise and bothering your neighbors
       </h3>
-      <img src="https://one-hit-wonders.s3.us-west-2.amazonaws.com/site_capture.png" className="screenshot"></img>
+      <div className="site-info-container">
+        <div className="instructions">
+          <p>1) Create samples</p>
+          <p>2) Upload samples into a kit</p>
+          <p className="fun-instruction">3) HIT STUFF</p>
+        </div>
+        <img
+          src="https://one-hit-wonders.s3.us-west-2.amazonaws.com/site_capture.png"
+          className="screenshot"
+        ></img>
+      </div>
     </>
   );
 };
